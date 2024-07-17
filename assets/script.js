0362d9a4c439d4633cf7787eb664787e2bc1344a
JuliaKardelice <julia.royer@live.fr>
2024-07-17 11:11:37 +0200

    second computer

--- assets/script.js
+++ assets/script.js
@@ -1,85 +1,63 @@
+
+///Tableau slides
+
 const slides = [
-	{
-		"image": "slide1.jpg",
-		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+	{ ///objet image
+	"image":"slide1.jpg", ///parametre 1 ceci modifiera l'image
+	"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>" ///parametre 2 ceci modifiera le texte
 	},
 	{
-		"image": "slide2.jpg",
-		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	"image":"slide2.jpg",
+	"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image": "slide3.jpg",
-		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	"image":"slide3.jpg",
+	"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image": "slide4.png",
-		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+	"image":"slide4.png",
+	"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
-
-
-
-// Dots
-function generetedDots() {
-	for (let i = 0; i < slides.length; i++) {
-		const dotsParent = document.querySelector(".dots");
-		const dotChild = document.createElement("div");
-		dotChild.classList.add("dot");
-		dotsParent.appendChild(dotChild);
+	]
+	
+	let compteur = 0;
+	
+	///Variables flèches
+	const arrowLeft = document.querySelector(".arrow_left");
+	const arrowRight = document.querySelector(".arrow_right");
+	const bannerImg = document.querySelector('.banner-img');
+	const tagLine = document.querySelector('#banner p');
+	const dots = document.querySelectorAll(".dot");
+	
+	function changeImg(){
+	bannerImg.src = `assets/images/slideshow/${slides[compteur].image}`;
+	tagLine.innerHTML = slides[compteur].tagLine;
 	}
-}
-
-// Remove dots
-function removeDots() {
-	const dotChild = document.querySelectorAll(".dot");
 	
-	for (let i = 0; i < slides.length; i++) {
-		dotChild[i].classList.remove("dot_selected");
+	function changeDots(){
+	dots.forEach(dot => dot.classList.remove('dot_selected'));
+	dots[compteur].classList.add('dot_selected');
 	}
-	dotChild[slideIndex].classList.add("dot_selected");	
-}
-
-// Contenu 
-function generetedSlider(slides) {
 	
-	const imageSlides = document.querySelector(".banner-img");
-	imageSlides.src = "./assets/images/slideshow/" + slides[slideIndex].image;
-	
-	const taglineSlides = document.querySelector("#banner p");
-	taglineSlides.innerHTML = slides[slideIndex].tagLine;
-	
-	console.log(slides[slideIndex].image);
-}
-
-generetedDots();
-
-const test = document.querySelector(".dot");
-test.classList.add("dot_selected");	
-
-var slideIndex = 0;
-const slideShow = slides[slideIndex];
-var flecheGauche = document.querySelector(".arrow_left");
-
-// Fleches
-flecheGauche.onclick = function () {
-	if (slideIndex === 0) {
-		slideIndex = slides.length - 1
+	arrowLeft.addEventListener('click', function(){
+	compteur--;
+	if(compteur < 0){
+	compteur = slides.length-1;
 	}
-	else slideIndex--;
+	changeImg();
+	changeDots();
+	});
 	
-	generetedSlider(slides);
-	removeDots();
-}
-
-var flecheDroite = document.querySelector(".arrow_right");
-
-flecheDroite.onclick = function () {
-	if (slideIndex === slides.length - 1) {
-		slideIndex = 0
+	arrowRight.addEventListener('click', function(){
+	compteur++;
+	if(compteur == slides.length){
+	compteur = 0;
 	}
-	else slideIndex++;
+	changeImg();
+	changeDots();
+	});
 	
-	generetedSlider(slides);
-	removeDots();
-}
-
+	
+	
+	
+	
